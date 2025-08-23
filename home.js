46fc7e09d9274a7a0be2816e6bb492a7a4506366
Mahfuz <mahfuzru5634@gmail.com>
2025-08-23 11:16:37 +0600

    implement cashout section

--- home.js
+++ home.js
@@ -19,3 +19,31 @@ document
       alert("Wrong Pin Number");
     }
   });
+
+document
+  .getElementById("withdraw-money-button")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    let balanceElement = document.getElementById("total-balance");
+    let totalBalance = parseFloat(balanceElement.innerText);
+
+    let withdrawAmmount = parseFloat(
+      document.getElementById("amount-withdraw").value
+    );
+    let currentammount = totalBalance - withdrawAmmount;
+    balanceElement.innerText = currentammount;
+  });
+
+// toggleing
+document
+  .getElementById("add-money-button")
+  .addEventListener("click", function () {
+    document.getElementById("cashout-section").style.display = "none";
+    document.getElementById("add-money-section").style.display = "block";
+  });
+document
+  .getElementById("cashout-button")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-section").style.display = "none";
+    document.getElementById("cashout-section").style.display = "block";
+  });
